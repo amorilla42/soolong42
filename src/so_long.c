7f1c798379518d2e1048eafc343b37968d949d7b
amorilla42 <andresmm4242@gmail.com>
2023-03-12 14:34:10 +0100

    empezamos

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,8 @@
 /*   By: amorilla <amorilla@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 13:49:06 by amorilla          #+#    #+#             */
-/*   Updated: 2023/03/12 13:49:07 by amorilla         ###   ########.fr       */
+/*   Updated: 2023/03/12 13:56:43 by amorilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,16 @@
 /*   By: amorilla <amorilla@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 13:49:01 by amorilla          #+#    #+#             */
-/*   Updated: 2023/03/12 13:49:02 by amorilla         ###   ########.fr       */
+/*   Updated: 2023/03/12 13:55:17 by amorilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "libft.h"
+# include "get_next_line.h"
+# include "MLX42/MLX42.h"
+
+
+
+#endif
